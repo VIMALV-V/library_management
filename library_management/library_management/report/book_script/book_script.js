f0adea1ebac7f33121011643fb8ca766c548544a
Vimal V <vimal137msd@gmail.com>
2026-08-29 13:01:36 +0000

    Add Book Script Report with custom HTML template

---
+++ library_management/library_management/report/book_script/book_script.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2026, Vimal and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Book Script"] = {
+	filters: [
+		// {
+		// 	"fieldname": "my_filter",
+		// 	"label": __("My Filter"),
+		// 	"fieldtype": "Data",
+		// 	"reqd": 1,
+		// },
+	],
+};
